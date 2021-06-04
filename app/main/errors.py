62c4d7c532b9dae7ae133ae424b2594f97fef8b8
ndanu-josy <josphinendanu3@gmail.com>
2021-06-04 23:03:40 +0300

    Articles and sources classes in models file

--- app/main/errors.py
+++ app/main/errors.py
@@ -6,4 +6,4 @@ def four_Ow_four(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('fourowfour.html'),404
+    return render_template('fourOwFour.html'),404
