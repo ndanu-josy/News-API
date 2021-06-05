e8f103a8a5a059697f2716800667cf071baeacdd
ndanu-josy <josphinendanu3@gmail.com>
2021-06-05 22:13:26 +0300

    Making changes

--- app/main/views.py
+++ app/main/views.py
@@ -9,13 +9,13 @@ def index():
 	'''
 	view root page function that returns the index the page and its data
 	'''
-	sources = get_sources('business')
+	business_sources = get_sources('business')
 	sports_sources = get_sources('sports')
 	technology_sources = get_sources('technology')
 	entertainment_sources = get_sources('entertainment')
-	title = "News Highlighter"
+	title = "E-NEWS"
 
-	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
+	return render_template('index.html',title = title, business_sources = business_sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
 @main.route('/sources/<id>')
 def articles(id):
